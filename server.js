25e9c4efa3869292d37391a1a7a539aa29bda9af
ehsan <ehsankhakian@hotmail.com>
2018-03-18 13:15:37 +0330

    fix6

--- server.js
+++ server.js
@@ -22,88 +22,88 @@ bot.onText(/\/start/, (msg) => {
         "لطفا  انتخاب کنید",{
         'reply_markup':BTN1GP,
     })
-  return true
+    bot.on('message', (msg) => {
+        fs.readFile(`./data/USERS/${msg.from.username}.json` , function (err, data) {
+        if (data)
+        var userData = JSON.parse(data);
+        console.log(userData);
+        if (msg.text == Btn1_RASM ) {
+            
+                if( userData.RASM.length >=23){
+                    
+                    userData.RASM=[];
+                    
+                }
+                var rasmArray = userData.RASM;
+              
+                do{
+                    var randomNumber = Math.ceil(Math.random() * 23);
+                }while(rasmArray.find((rasmNumber)=>randomNumber===rasmNumber));
+                userData.RASM.push(randomNumber)
+    
+                fs.writeFile(`./data/USERS/${msg.from.username}.json` , JSON.stringify(userData), (err) => {
+                    if (err) throw err;
+                
+                   
+                });
+                fs.readFile(`./data/RASM/${randomNumber}.txt`, 'utf8', function (err, data) {
+                    if (err) {
+                      
+                      return;
+                    }
+                    bot.sendMessage(
+                              msg.chat.id,
+                              data + "\n\n" + "از طرف  "+ msg.from.first_name  ,
+                              {parse_mode : "HTML"}
+                          );
+             
+                  });
+          
+          
+        } 
+        
+        if (msg.text == Btn1_DUST ) {
+           
+            
+                if( userData.DUST.length >=23){
+                    
+                    userData.DUST=[];
+                    
+                }
+                var dustArray = userData.DUST;
+              
+                do{
+                    var randomNumber = Math.ceil(Math.random() * 23);
+                }while(dustArray.find((dustNumber)=>randomNumber===dustNumber));
+                userData.DUST.push(randomNumber)
+    
+                fs.writeFile(`./data/USERS/${msg.from.username}.json` , JSON.stringify(userData), (err) => {
+                    if (err) throw err;
+                
+                   
+                });
+                fs.readFile(`./data/DUST/${randomNumber}.txt`, 'utf8', function (err, data) {
+                    if (err) {
+                      
+                      return;
+                    }
+                    bot.sendMessage(
+                              msg.chat.id,
+                              data + "\n\n" + "از طرف  "+ msg.from.first_name  ,
+                              {parse_mode : "HTML"}
+                          );
+             
+                  });
+            
+          
+          
+        }
+     })
+    });
   
 });
 
-bot.on('message', (msg) => {
-    fs.readFile(`./data/USERS/${msg.from.username}.json` , function (err, data) {
-    if (data)
-    var userData = JSON.parse(data);
-    console.log(userData);
-    if (msg.text == Btn1_RASM ) {
-        
-            if( userData.RASM.length >=23){
-                
-                userData.RASM=[];
-                
-            }
-            var rasmArray = userData.RASM;
-          
-            do{
-                var randomNumber = Math.ceil(Math.random() * 23);
-            }while(rasmArray.find((rasmNumber)=>randomNumber===rasmNumber));
-            userData.RASM.push(randomNumber)
 
-            fs.writeFile(`./data/USERS/${msg.from.username}.json` , JSON.stringify(userData), (err) => {
-                if (err) throw err;
-            
-               
-            });
-            fs.readFile(`./data/RASM/${randomNumber}.txt`, 'utf8', function (err, data) {
-                if (err) {
-                  
-                  return;
-                }
-                bot.sendMessage(
-                          msg.chat.id,
-                          data + "\n\n" + "از طرف  "+ msg.from.first_name  ,
-                          {parse_mode : "HTML"}
-                      );
-         
-              });
-      
-      
-    } 
-    
-    if (msg.text == Btn1_DUST ) {
-       
-        
-            if( userData.DUST.length >=23){
-                
-                userData.DUST=[];
-                
-            }
-            var dustArray = userData.DUST;
-          
-            do{
-                var randomNumber = Math.ceil(Math.random() * 23);
-            }while(dustArray.find((dustNumber)=>randomNumber===dustNumber));
-            userData.DUST.push(randomNumber)
-
-            fs.writeFile(`./data/USERS/${msg.from.username}.json` , JSON.stringify(userData), (err) => {
-                if (err) throw err;
-            
-               
-            });
-            fs.readFile(`./data/DUST/${randomNumber}.txt`, 'utf8', function (err, data) {
-                if (err) {
-                  
-                  return;
-                }
-                bot.sendMessage(
-                          msg.chat.id,
-                          data + "\n\n" + "از طرف  "+ msg.from.first_name  ,
-                          {parse_mode : "HTML"}
-                      );
-         
-              });
-        
-      
-      
-    }
- })
-});
 
 
 
